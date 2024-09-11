39016a4ee3849f391046f6f7ba9d4b05b164341a
nvim <prozskov@gmail.com>
2024-09-11 12:41:11 +0200

    Remove helpers for the clean ts tests
    Ref: https://github.com/beda-software/fhir-client-testing-tool/issues/7

--- packages/client-testing-demo-tests-standard/src/index.ts
+++ packages/client-testing-demo-tests-standard/src/index.ts
@@ -2,34 +2,6 @@ import { Patient } from 'fhir/r4';
 import { Request } from '@beda.software/app/src/modules/requests/request.entity';
 import { isResourceValid } from '@beda.software/fhir-validator';
 
-function patientRequestsOnlyAvailableInteractionsExists(requests: Request[]): boolean {
-    const availableInteractions = ['READ', 'SEARCH', 'CREATE', 'UPDATE'];
-    const filteredRequests = requests.filter((request) => !availableInteractions.includes(request.fhirAction));
-
-    return filteredRequests.length === 0;
-}
-
-function patientRequestsOnlyAvailableSearchParamsExists(requests: Request[]): boolean {
-    const availableSearchParams = [
-        '_id',
-        'birthdate',
-        'family',
-        'gender',
-        'identifier',
-        'name',
-        'gender-identity',
-        'indigenous-status',
-    ];
-
-    return checkAvailableParams(availableSearchParams, false, requests);
-}
-
-function patientRequestsOnlyAvailableComboSearchParamsExists(requests: Request[]): boolean {
-    const availableComboSearchParams = ['birthdate+family', 'birthdate+name', 'gender+name', 'family+gender'];
-
-    return checkAvailableParams(availableComboSearchParams, true, requests);
-}
-
 function checkAvailableParams(availableParams: string[], combo: boolean, requests: Request[]): boolean {
     const filterCombo = (param: string) => (combo ? param.includes('+') : !param.includes('+'));
     const usedComboSearchParams = requests
@@ -39,18 +11,6 @@ function checkAvailableParams(availableParams: string[], combo: boolean, request
     const uniqueUsedComboSearchParams = [...new Set(usedComboSearchParams)];
 
     return uniqueUsedComboSearchParams.every((searchParam) => availableParams.includes(searchParam));
-}
-
-async function patientRequestCreateValidPatient(requests: Request[]): Promise<boolean> {
-    const filteredRequests = requests.filter(
-        (request) => request.fhirAction === 'CREATE' && request.requestBody?.resourceType === 'Patient',
-    );
-    const validationStatuses = await Promise.all(
-        filteredRequests.map(async (request) => await isResourceValid(request.requestBody as Patient)),
-    );
-    const falseValidations = validationStatuses.filter((status) => status === false);
-
-    return falseValidations.length === 0;
 }
 
 export function patientDemoTest() {
@@ -65,21 +25,43 @@ export function patientDemoTest() {
         });
 
         test('Should only have available interactions', async () => {
-            expect(patientRequestsOnlyAvailableInteractionsExists(requests)).toBe(true);
+            const availableInteractions = ['READ', 'SEARCH', 'CREATE', 'UPDATE'];
+            const filteredRequests = requests.filter((request) => !availableInteractions.includes(request.fhirAction));
+
+            expect(filteredRequests.length).toBe(0);
         });
 
         test('Should only have available search params', async () => {
-            expect(patientRequestsOnlyAvailableSearchParamsExists(requests)).toBe(true);
+            const availableSearchParams = [
+                '_id',
+                'birthdate',
+                'family',
+                'gender',
+                'identifier',
+                'name',
+                'gender-identity',
+                'indigenous-status',
+            ];
+
+            expect(checkAvailableParams(availableSearchParams, false, requests)).toBe(true);
         });
 
         test('Should only have available combo search params', async () => {
-            expect(patientRequestsOnlyAvailableComboSearchParamsExists(requests)).toBe(true);
+            const availableComboSearchParams = ['birthdate+family', 'birthdate+name', 'gender+name', 'family+gender'];
+
+            expect(checkAvailableParams(availableComboSearchParams, true, requests)).toBe(true);
         });
 
         test('Should only have valid resources in CREATE action', async () => {
-            expect(
-                await patientRequestCreateValidPatient(requests.filter((request) => request.fhirAction === 'CREATE')),
-            ).toBe(true);
+            const filteredRequests = requests.filter(
+                (request) => request.fhirAction === 'CREATE' && request.requestBody?.resourceType === 'Patient',
+            );
+            const validationStatuses = await Promise.all(
+                filteredRequests.map(async (request) => await isResourceValid(request.requestBody as Patient)),
+            );
+            const falseValidations = validationStatuses.filter((status) => status === false);
+
+            expect(falseValidations.length).toBe(0);
         });
     });
 }
